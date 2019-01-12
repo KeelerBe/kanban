0fbe472f85f563090d0c672ef5d602c37f437374
Keeler Mendoza <keelerbe@gmail.com>
2019-01-12 17:16:18 +0800

    Handle fetching errors

--- client/src/utils/client.js
+++ client/src/utils/client.js
@@ -2,23 +2,23 @@ import axios from 'axios'
 
 const getTasks = (onSuccess) => {
   axios.get('/api/tasks')
-    .then((response) => response.data)
-    .then(onSuccess)
-    .catch((error) => console.log(error))
+    .then(checkStatus)
+    .then((response) => onSuccess(response.data))
+    .catch(onError)
 }
 
 const addTask = (task) => {
   axios.post('/api/tasks', { task })
-    .then((response) => response)
-    .catch((error) => console.log(error))
+    .then(checkStatus)
+    .catch(onError)
 }
 
 const deleteTask = (taskId) => {
   axios.delete('/api/tasks', {
     data: { id: taskId }
   })
-    .then((response) => response)
-    .catch((error) => console.log(error))
+    .then(checkStatus)
+    .catch(onError)
 }
 
 const updateTask = (taskId, newTitle) => {
@@ -26,8 +26,23 @@ const updateTask = (taskId, newTitle) => {
     id: taskId,
     title: newTitle
   })
-    .then((response) => response)
-    .catch((error) => console.log(error))
+    .then(checkStatus)
+    .catch(onError)
+}
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+   return response
+  }
+}
+
+const onError = ({ response }) => {
+  const { status, statusText } = response.request
+  const error = new Error(`HTTP Error: ${status} ${statusText}`)
+  error.status = statusText
+  error.response = response
+  console.log(error)
+  throw error
 }
 
 export default {
